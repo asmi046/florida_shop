88a1a1b0f3294f0b11d19a67c10b60192553b94b
asmi046 <asmi046@gmail.com>
2023-02-09 17:38:13 +0300

    Страница контактов

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -15,6 +15,24 @@
         <x-breadcrumbs :title="$title" ></x-breadcrumbs>
         <h1 class="h1_page">{{$title}}</h1>
 
+        <div class="contacts_wrapper">
+            <p class="sub_h">Адрес магазина</p>
+            <p class="param">{{$options['adress']}}</p>
+
+            <p class="sub_h">Контактный телефон</p>
+            <p class="param">{{$options['phone']}}</p>
+
+            <p class="sub_h">Мы в социальных сетях</p>
+            <x-messanger></x-messanger>
+
+            <p class="sub_h">Реквизиты</p>
+            <div class="text_blk">
+                ИП Иванов И. И<br/>
+                Карла Маркса, 72 корпус 18<br/>
+                ИНН:<br/>
+                ОГРН:<br/>
+            </div>
+        </div>
     </div>
 </section>
 
