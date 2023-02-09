3ee6197276cf3a692cb6f703d6cc3db2d8e4163e
asmi046 <asmi046@gmail.com>
2023-02-09 19:46:22 +0300

    Верстка блога

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -14,6 +14,21 @@
     <div class="_wrapper">
         <x-breadcrumbs :title="$title" ></x-breadcrumbs>
         <h1 class="h1_page">{{$title}}</h1>
+        <div class="blog_in_page">
+            @for ($i = 0; $i<12; $i++)
+                <a href="" class="blog_blk">
+                    <div class="img_wrp">
+                        <img src="{{asset('img/facer_img/tovars/tov_1.jpg')}}" alt="">
+                    </div>
+                    <h2>Топ - 5 Экзотических цветов в сборных композициях</h2>
+                    <p class="data">09.08.2022</p>
+                    <p class="descr">1 Эрингиум Оригинальные сборные букеты можно создать именно с этим цветком. Соцветия эрингиума похожи на чертополох, только синего оттенка. Причудливый</p>
+                </a>
+            @endfor
+
+        </div>
+
+        {{-- <x-pagination :tovars="$reviews"></x-pagination> --}}
 
     </div>
 </section>
