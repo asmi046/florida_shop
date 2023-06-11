f2ea58f2d5e8aaebe2cb224dd70e4edb3dbb3558
asmi046 <asmi046@gmail.com>
2023-06-12 00:43:55 +0300

    Пустые фото в отзывах

--- resources/views/components/rew-blk.blade.php
+++ resources/views/components/rew-blk.blade.php
@@ -1,7 +1,16 @@
 <div class="rew_blk">
-    <div class="foto_wrapper">
-        <img src="{{$item['avatar']}}" alt="Отзвыв от покупателя: {{$item['name']}}">
-    </div>
+    @if (empty($item['avatar']))
+        <div class="foto_text_wrapper">
+            <span>{{mb_substr(strtoupper($item['name']), 0, 1)}}</span>
+        </div>
+    @else
+
+        <div class="foto_wrapper">
+            <img src="{{$item['avatar']}}" alt="Отзвыв от покупателя: {{$item['name']}}">
+        </div>
+
+    @endif
+
 
     <div class="text_wrapper">
         <h3>{{$item['name']}}</h3>
