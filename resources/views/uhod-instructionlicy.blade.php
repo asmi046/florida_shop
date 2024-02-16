c788003112ad6c40cf507874da40cedde6380dab
asmi046 <asmi046@gmail.com>
2024-02-16 10:39:13 +0300

    +1

--- resources/views/uhod-instructionlicy.blade.php
+++ resources/views/uhod-instructionlicy.blade.php
@@ -19,7 +19,7 @@
             <p>Чтобы радость от подаренных вам цветов оставалась яркой и долгой, мы подготовили для вас небольшую памятку по уходу за разными видами цветов. Следуя этим рекомендациям, вы сможете наслаждаться красотой наших букетов дольше.</p>
 
             <div class="М">
-                <iframe id="vk_frame" src="https://vk.com/video_ext.php?oid=-81186664&id=456239361&hd=2" width="100%" allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
+                <iframe id="vk_frame" src="https://vk.com/video_ext.php?oid=-81186664&id=456239361&hd=2" allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
             </div>
 
             <style>
