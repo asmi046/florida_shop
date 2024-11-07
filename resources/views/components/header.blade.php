f517b05f4d9eaad12ad3d433f8b909e5e7a90fab
asmi046 <asmi046@gmail.com>
2024-11-07 03:46:49 +0300

    Шапка полусверстанная

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,28 @@
-<header id="header">
+<header class="header" id="header">
+    <div class="_wrapper">
+        <div class="top">
+            <a class="logo" href="{{route('home')}}">
+                <img src="{{asset('img/logo_new_white.svg')}}" alt="Florida - Курск">
+            </a>
 
+            <x-main-menu></x-main-menu>
+
+            <div class="shop_control">
+                <x-icon-a href="#" ancor="Поиск" icon="search"></x-icon-a>
+                <x-icon-a href="#" ancor="Корзина" icon="cart"></x-icon-a>
+                <x-icon-a href="#" ancor="Кабинет" icon="shop"></x-icon-a>
+            </div>
+        </div>
+
+        <div class="bottom">
+            <h1>Доставка цветов <br>в Курске</h1>
+            <span class="plash">
+                Кругласуточно 24/7
+            </span>
+            <p>Гарантия на цветы — 48 часов <br>или заменим букет!</p>
+        </div>
+    </div>
+    {{--
         <div class="top_line">
 
             <div class="side left_side">
@@ -16,22 +39,18 @@
                     <a href="{{route('login')}}" class="cabinet pi florida_cabinet">Войти</a>
                 @endguest
             </div>
-        </div>
+        </div> --}}
 
-
+{{--
     <div class="_wrapper">
         <a class="logo" href="{{route('home')}}">
             <img src="{{asset('img/logo_main.svg')}}" alt="Florida - Курск">
         </a>
-
-
-        {{-- <a href="{{route('contacts')}}" class="addres_head pi florida_map_pin">{{$options['adress_fk']}}</a> --}}
-
 
         <x-main-menu></x-main-menu>
 
         <x-messanger></x-messanger>
 
 
-    </div>
+    </div> --}}
 </header>
