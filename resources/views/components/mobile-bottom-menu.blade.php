8c5528cd06097cc56b3362a3fbba2e8322b447f7
asmi046 <asmi046@gmail.com>
2024-11-11 19:17:47 +0300

    Меню мобильное

--- resources/views/components/mobile-bottom-menu.blade.php
+++ resources/views/components/mobile-bottom-menu.blade.php
@@ -1,25 +1,25 @@
 <div class="mobile_bottom_menu">
 
     <a href="{{route('home')}}" class="bm_btn">
-        <div class="icon bottom_menu_icon_home  pi florida_home"></div>
+        <svg class="sprite_icon">
+            <use xlink:href="#rose"></use>
+        </svg>
         <p>Главная</p>
     </a>
 
-    <a href="{{route('favorites')}}" class="bm_btn">
-        <div class="icon bottom_menu_icon_favorites pi florida_like"></div>
-        <p>Избранное</p>
-    </a>
+    <a href="{{route('bascet')}}" class="bm_btn bm_btn_cart">
 
-    <a href="{{route('bascet')}}" class="bm_btn">
-        <div class="icon bascet_blk bottom_menu_icon_bascet pi florida_cart">
-            {{-- <span class="bascet_counter">0</span> --}}
-            <bascet-counter></bascet-counter>
-        </div>
+        <bascet-counter></bascet-counter>
+        <svg class="sprite_icon">
+            <use xlink:href="#cart"></use>
+        </svg>
         <p>Корзина</p>
     </a>
 
     <a href="tel:{{str_replace(array('-', ' ', '(' , ')'), '', $options['phone'])}}" class="bm_btn open_cat_menu ">
-        <div class="icon bottom_menu_icon_phone pi florida_phone"></div>
+        <svg class="sprite_icon">
+            <use xlink:href="#phone"></use>
+        </svg>
         <p>Позвонить</p>
     </a>
 
