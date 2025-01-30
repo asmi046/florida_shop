05fff81f8d9533193213f2ea72cf84ea89c60ef2
asmi046 <asmi046@gmail.com>
2025-01-30 19:12:07 +0300

    Доп видео в странице

--- resources/views/uhod-instructionlicy.blade.php
+++ resources/views/uhod-instructionlicy.blade.php
@@ -19,6 +19,10 @@
             <p>Чтобы радость от подаренных вам цветов оставалась яркой и долгой, мы подготовили для вас небольшую памятку по уходу за разными видами цветов. Следуя этим рекомендациям, вы сможете наслаждаться красотой наших букетов дольше.</p>
 
             <div class="thumb-wrap">
+                <iframe src="https://vkvideo.ru/video_ext.php?oid=-81186664&id=456239492&hd=2&autoplay=1" width="853" height="480" allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;" frameborder="0" allowfullscreen></iframe>
+            </div>
+
+            <div class="thumb-wrap">
                 <iframe id="vk_frame" src="https://vk.com/video_ext.php?oid=-81186664&id=456239361&hd=2" width="853" height="480" allow="autoplay; encrypted-media; fullscreen; picture-in-picture;" frameborder="0" allowfullscreen></iframe>
             </div>
 
