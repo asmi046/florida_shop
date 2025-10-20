049e7a3ab4423064930c007ba78989b58cb9c64c
asmi046 <asmi046@gmail.com>
2025-10-21 01:15:01 +0300

    Убрали блог

--- resources/views/components/main-menu.blade.php
+++ resources/views/components/main-menu.blade.php
@@ -1,7 +1,7 @@
 <ul class="main_menu">
     <li><a href="{{route('catalog')}}">Букеты</a></li>
     <li><a href="{{route('reviews')}}">Отзывы</a></li>
-    <li><a href="{{route('blog')}}">Блог</a></li>
+    {{-- <li><a href="{{route('blog')}}">Блог</a></li> --}}
     <li><a href="{{route('zones')}}">Доставка и оплата</a></li>
     <li><a href="{{route('contacts')}}">Контакты</a></li>
 </ul>
