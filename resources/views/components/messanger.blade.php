8f218391938a002954d0146bdc8518403ac41728
asmi046 <asmi046@gmail.com>
2025-10-20 19:15:04 +0300

    Разметка заголовка

--- resources/views/components/messanger.blade.php
+++ resources/views/components/messanger.blade.php
@@ -5,19 +5,19 @@
 </div> --}}
 
 <div class="social_line messendger_head_blk">
-    <a href="{{$options['vk_lnk']}}" class="link vk_lnk">
+    <a href="{{strip_tags($options['vk_lnk'])}}" class="link vk_lnk">
         <svg class="sprite_icon vk_icon">
             <use xlink:href="#vk_icon"></use>
         </svg>
     </a>
 
-    <a href="{{$options['telegram_lnk']}}" class="link tg_lnk">
+    <a href="{{strip_tags($options['telegram_lnk'])}}" class="link tg_lnk">
         <svg class="sprite_icon">
             <use xlink:href="#tg_icon"></use>
         </svg>
     </a>
 
-    <a href="{{$options['whatsapp_lnk']}}" class="link ws_lnk">
+    <a href="{{strip_tags($options['whatsapp_lnk'])}}" class="link ws_lnk">
         <svg class="sprite_icon">
             <use xlink:href="#ws_icon"></use>
         </svg>
