442d6268b373602c039238b282b37c86694c105b
asmi046 <asmi046@gmail.com>
2026-01-06 18:48:01 +0300

    Обновление laravel

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,23 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\Review;
 
-use Illuminate\Http\Request;
-
 class IndexController extends Controller
 {
-    public function show() {
+    public function show()
+    {
 
         $all_product = Product::paginate(9);
         $sales_liders = Product::select()->orderBy('sales_count')->take(8)->get();
-        $sales = Product::where('old_price', '!=', 0)->orWhere("hit", 1)->orderBy("created_at", "DESC")->take(8)->get();
+        $sales = Product::where('old_price', '!=', 0)->orWhere('hit', 1)->orderBy('created_at', 'DESC')->take(8)->get();
 
         $news = Product::where('new', true)->take(8)->get();
         $hits = Product::where('hit', true)->take(8)->get();
 
         $reviews = Review::select()->take(9)->get();
 
-        if (count($sales) == 0)
+        if (count($sales) == 0) {
             $sales = Product::where('hit', true)->take(8)->get();
+        }
 
         return view('index', [
             'all_product' => $all_product,
--- resources/views/layouts/all.blade.php
+++ resources/views/layouts/all.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <title>@yield('title') - Florida</title>
     <meta name="description" content="@yield('description')">
@@ -8,53 +9,65 @@
     <meta property="og:type" content="website" />
     <meta property="og:title" content="@yield('title')" />
     <meta property="og:description" content="@yield('description')" />
-    <meta property="og:url" content="{{route('home')}}" />
+    <meta property="og:url" content="{{ route('home') }}" />
     <meta property="og:site_name" content="Магазин цветов Florida 46" />
     <meta property="og:image" content="@yield('img', asset('img/og-img.jpg'))" />
     <meta property="og:image:type" content="image/jpeg" />
     <meta name="twitter:card" content="summary_large_image" />
 
-	<meta charset="UTF-8">
-	<meta name="format-detection" content="telephone=no">
+    <meta charset="UTF-8">
+    <meta name="format-detection" content="telephone=no">
 
-    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon256.png')}}" sizes="256x256">
-    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon128.png')}}" sizes="128x128">
-    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon64.png')}}" sizes="64x64">
-    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon32.png')}}" sizes="32x32">
-    <link rel="icon" type="image/png" href="{{asset('/img/favicons/icon16.png')}}" sizes="16x16">
-    <link rel="icon" type="image/svg" href="{{asset('/img/favicons/logo-mini.svg')}}" sizes="any">
+    <link rel="icon" type="image/png" href="{{ asset('/img/favicons/icon256.png') }}" sizes="256x256">
+    <link rel="icon" type="image/png" href="{{ asset('/img/favicons/icon128.png') }}" sizes="128x128">
+    <link rel="icon" type="image/png" href="{{ asset('/img/favicons/icon64.png') }}" sizes="64x64">
+    <link rel="icon" type="image/png" href="{{ asset('/img/favicons/icon32.png') }}" sizes="32x32">
+    <link rel="icon" type="image/png" href="{{ asset('/img/favicons/icon16.png') }}" sizes="16x16">
+    <link rel="icon" type="image/svg" href="{{ asset('/img/favicons/logo-mini.svg') }}" sizes="any">
 
     <meta name="_token" content="{{ csrf_token() }}">
 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="{{asset('js/lib/swiper/swiper-bundle.min.css')}}"/>
-    <script src="{{asset('js/lib/swiper/swiper-bundle.min.js')}}"></script>
-    <script src="{{asset('js/lib/imask.js')}}"></script>
+    <link rel="stylesheet" href="{{ asset('js/lib/swiper/swiper-bundle.min.css') }}" />
+    <script src="{{ asset('js/lib/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('js/lib/imask.js') }}"></script>
 
     <script src="//api-maps.yandex.ru/2.1/?lang=ru_RU&coordorder=longlat" type="text/javascript"></script>
 
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();
-    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                    return;
+                }
+            }
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(
+                k, a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-    ym(94073132, "init", {
-         clickmap:true,
-         trackLinks:true,
-         accurateTrackBounce:true,
-         webvisor:true,
-         ecommerce:"dataLayerEc"
-    });
-    window.dataLayerEc = window.dataLayerEc || [];
- </script>
- <noscript><div><img src="https://mc.yandex.ru/watch/94073132" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
- <!-- /Yandex.Metrika counter -->
+        ym(94073132, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true,
+            ecommerce: "dataLayerEc"
+        });
+        window.dataLayerEc = window.dataLayerEc || [];
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/94073132" style="position:absolute; left:-9999px;" alt="" />
+        </div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
 
- <meta name="yandex-verification" content="8e9f290ab57d976f" />
+    <meta name="yandex-verification" content="8e9f290ab57d976f" />
 
     {{-- <script src="//cdn.callibri.ru/callibri.js" type="text/javascript" charset="utf-8"></script> --}}
 
@@ -63,36 +76,12 @@
         window.storage = "{{ asset('storage') }}";
     </script>
 
-    @vite([
-        'resources/css/app.css',
-        'public/fonts/icons/style.css',
-        'public/css/main.scss',
-        'public/css/tovar_filter.css',
-        'public/css/tovar_page_content.css',
-        'public/css/catalog_menu.css',
-        'public/css/cart.css',
-        'public/css/auth.css',
-        'public/css/cabinet.css',
-        'public/css/mainsearch.css',
-        'public/css/pagination.css',
-
-
-        'resources/js/app.js',
-        'public/js/sliders.js',
-        'public/js/delivery_zone.js',
-        'public/js/filter.js',
-        'public/js/map.js',
-        'public/js/categories.js',
-        'public/js/catalog_menu.js',
-        'public/js/cart.js',
-        'public/js/favorites.js',
-        'public/js/mainsearch.js',
-    ])
+    @vite(['resources/css/app.css', 'public/fonts/icons/style.css', 'public/css/main.scss', 'public/css/tovar_filter.css', 'public/css/tovar_page_content.css', 'public/css/catalog_menu.css', 'public/css/cart.css', 'public/css/auth.css', 'public/css/cabinet.css', 'public/css/mainsearch.css', 'public/css/pagination.css', 'resources/js/app.js', 'public/js/sliders.js', 'public/js/delivery_zone.js', 'public/js/filter.js', 'public/js/map.js', 'public/js/categories.js', 'public/js/catalog_menu.js', 'public/js/cart.js', 'public/js/favorites.js', 'public/js/mainsearch.js'])
 
     <script type="application/ld+json">
     {
-        "@context": "https://schema.org",
-        "@type": "Florist",
+        "@@context": "https://schema.org",
+        "@@type": "Florist",
         "name": "Магазин цветов «Florida»",
         "image": "https://florida46.ru/img/favicons/icon256.png",
         "url": "https://florida46.ru",
@@ -117,21 +106,24 @@
         <x-schema-org.product-offer :product="$product"></x-schema-org.product-offer>
     @endisset
 
-
 </head>
 
 <body>
-    @include("allicon")
+    @include('allicon')
     <x-catalog-menu></x-catalog-menu>
     <x-mobile-bottom-menu></x-mobile-bottom-menu>
 
 
 
-	<div class="wrapper" id="global_app">
+    <div class="wrapper" id="global_app">
 
-        <modal-window rout="/send_consult" redirect="/thencs" hesh="floristConsult" title="Помощь флориста" subtitle="Мы свяжемся с Вами в течении 15 минут" showarea="false"></modal-window>
-        <modal-window rout="/send_review" redirect="/thencs" hesh="showModalRewiew" title="Оставить отзыв" subtitle="Нам важно Ваше мнение! Ваш отзыв поможет нам стать лучьше!" showarea="true"></modal-window>
-        <modal-window rout="/send_consult" redirect="/thencs" hesh="showModalNal" title="Уточнить наличие" subtitle="Укажите ваше имя и телефон. В течение нескольких минут с вами свяжется наш флорист для уточнения наличия" showarea="false"></modal-window>
+        <modal-window rout="/send_consult" redirect="/thencs" hesh="floristConsult" title="Помощь флориста"
+            subtitle="Мы свяжемся с Вами в течении 15 минут" showarea="false"></modal-window>
+        <modal-window rout="/send_review" redirect="/thencs" hesh="showModalRewiew" title="Оставить отзыв"
+            subtitle="Нам важно Ваше мнение! Ваш отзыв поможет нам стать лучьше!" showarea="true"></modal-window>
+        <modal-window rout="/send_consult" redirect="/thencs" hesh="showModalNal" title="Уточнить наличие"
+            subtitle="Укажите ваше имя и телефон. В течение нескольких минут с вами свяжется наш флорист для уточнения наличия"
+            showarea="false"></modal-window>
         <one-click-buy-window></one-click-buy-window>
 
 
@@ -147,7 +139,8 @@
                         </svg>
                     </div>
                     <div class="text">
-                        <p class="free">Бесплатная доставка распространяется на: проспект Победы, Дериглазова, ул. Карла-Маркса</p>
+                        <p class="free">Бесплатная доставка распространяется на: проспект Победы, Дериглазова, ул.
+                            Карла-Маркса</p>
                         <p class="pay">Доставка в другие районы: 150-250 р.</p>
                         <p class="pay">Доставка ко времени: 500 р.</p>
                     </div>
