3e97a98ca0b8df8deedf7cc4cb2b15592e5ccf70
asmi046 <asmi046@gmail.com>
2026-01-12 03:11:04 +0300

    Изменени в бегущей строке + перекраска поиска

--- resources/views/components/merquee.blade.php
+++ resources/views/components/merquee.blade.php
@@ -1,14 +1,14 @@
 <div class="marquee_section">
     <div class="marquee">
         <div class="marquee__content">
-        @for ($i=0; $i<6; $i++)
-            <p>Бесплатная доставка 📦</p>
-            <p class="separator">/</p>
-            <p>Подарок к каждому заказу 🎁</p>
-            <p class="separator">/</p>
-            <p>Программа лояльности ✨</p>
-            <p class="separator">/</p>
-        @endfor
+            @for ($i = 0; $i < 6; $i++)
+                <p>Бесплатная доставка 📦</p>
+                <p class="separator">/</p>
+                <p>В каждом заказе открытка 🎁</p>
+                <p class="separator">/</p>
+                <p>Программа лояльности - оплата до 30% ✨</p>
+                <p class="separator">/</p>
+            @endfor
         </div>
     </div>
 </div>
