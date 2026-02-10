6a7e64428fa4517f2609c562c43caeb200a86aa1
asmi046 <asmi046@gmail.com>
2026-02-10 14:45:55 +0300

    Проверка наличия категории у товара

--- app/Http/Controllers/TovarController.php
+++ app/Http/Controllers/TovarController.php
@@ -2,38 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Models\Product;
-
-use Illuminate\Support\Facades\Storage;
 
 class TovarController extends Controller
 {
-
-    public function getPriductById($id) {
+    public function getPriductById($id)
+    {
         $prosuct = Product::with(['tovar_categories', 'product_images'])->where('id', $id)->first();
 
         $main_img = asset('img/noPhoto.jpg');
 
-        if (!empty($prosuct->img))
+        if (! empty($prosuct->img)) {
             $main_img = $prosuct->img;
+        }
 
-        return ["product" => $prosuct, "main_img" => $main_img];
+        return ['product' => $prosuct, 'main_img' => $main_img];
     }
 
-    public function show($slug) {
+    public function show($slug)
+    {
 
         $prosuct = Product::with(['tovar_categories', 'product_images'])->where('slug', $slug)->first();
 
-        if($prosuct == null) abort('404');
+        if ($prosuct == null) {
+            abort('404');
+        }
 
         $images = $prosuct->product_images;
         // $up_sale = Product::where('category', $prosuct->category)->take(5)->get();
         $up_sale_cat = $prosuct->tovar_categories()->first();
-        $up_sale = $up_sale_cat->category_tovars()->take(5)->get();
+        if ($up_sale_cat == null) {
+            $up_sale = [];
+        } else {
+            $up_sale = $up_sale_cat->category_tovars()->take(5)->get();
+        }
 
-
-        return view('tovar', ['product' => $prosuct, "images" => $images, "upsale" => $up_sale]);
+        return view('tovar', ['product' => $prosuct, 'images' => $images, 'upsale' => $up_sale]);
     }
 }
