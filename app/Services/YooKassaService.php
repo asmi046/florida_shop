de86b3c941bf696af846c0ed095163a55812e2a9
asmi046 <asmi046@gmail.com>
2026-02-27 00:07:55 +0300

    Касса

--- app/Services/YooKassaService.php
+++ app/Services/YooKassaService.php
@@ -1,56 +1,53 @@
 <?php
+
 namespace App\Services;
 
-use YooKassa\Client;
-use App\Models\Order;
 use App\Actions\TelegramSendAction;
+use App\Models\Order;
 use Illuminate\Support\Facades\Log;
-use YooKassa\Model\Notification\NotificationFactory;
+use YooKassa\Client;
 use YooKassa\Model\Notification\NotificationEventType;
+use YooKassa\Model\Notification\NotificationFactory;
 
-class YooKassaService {
-
-
-
-    public function pay_fixation() {
+class YooKassaService
+{
+    public function pay_fixation()
+    {
 
         try {
             $source = file_get_contents('php://input');
             $data = json_decode($source, true);
 
-            $factory = new NotificationFactory();
+            $factory = new NotificationFactory;
             $notificationObject = $factory->factory($data);
             $responseObject = $notificationObject->getObject();
 
-            $client = new \YooKassa\Client();
+            $client = new \YooKassa\Client;
 
-            if (!config('yookassa.shop_id')) {
-                if (!$client->isNotificationIPTrusted($_SERVER['REMOTE_ADDR'])) {
+            if (! config('yookassa.shop_id')) {
+                if (! $client->isNotificationIPTrusted($_SERVER['REMOTE_ADDR'])) {
                     header('HTTP/1.1 400 Something went wrong (IP)');
                     exit();
                 }
             }
 
-
             if ($notificationObject->getEvent() === NotificationEventType::PAYMENT_SUCCEEDED) {
-                $someData = array(
+                $someData = [
                     'paymentId' => $responseObject->getId(),
                     'paymentStatus' => $responseObject->getStatus(),
-                );
-                Log::channel('pay')->info('Payment succeeded: '. $responseObject->getId());
+                ];
+                Log::channel('pay')->info('Payment succeeded: '.$responseObject->getId());
 
-            }  elseif ($notificationObject->getEvent() === NotificationEventType::PAYMENT_CANCELED) {
-                $someData = array(
+            } elseif ($notificationObject->getEvent() === NotificationEventType::PAYMENT_CANCELED) {
+                $someData = [
                     'paymentId' => $responseObject->getId(),
                     'paymentStatus' => $responseObject->getStatus(),
-                );
-                Log::channel('pay')->info('Payment concled: '. $responseObject->getId());
-            }
-            else {
+                ];
+                Log::channel('pay')->info('Payment concled: '.$responseObject->getId());
+            } else {
                 header('HTTP/1.1 400 Something went wrong (Type)');
                 exit();
             }
-
 
             $client->setAuth(config('yookassa.shop_id'), config('yookassa.secret_key'));
 
@@ -62,43 +59,42 @@ class YooKassaService {
                     $order->pay_status_text = $someData['paymentStatus'];
                     $order->save();
 
-                        $orderStatusText = ($someData['paymentStatus'] === "succeeded")?"Оплачен":"Не оплачен";
+                    $orderStatusText = ($someData['paymentStatus'] === 'succeeded') ? 'Оплачен' : 'Не оплачен';
 
-                        $pay_text = "<b>Заказ №".$order->id." ".$orderStatusText." </b>\n\r";
-                        $pay_text .= "<b>ID: </b>".$someData['paymentId']."\n\r";
-                        $pay_text .= "<b>Сумма: </b>".floatval($paymentInfo["amount"]["value"])." ₽\n\r";
-                        $tgsender = new TelegramSendAction();
-                        $tgsender->handle($pay_text);
+                    $pay_text = '<b>Заказ №'.$order->id.' '.$orderStatusText." </b>\n\r";
+                    $pay_text .= '<b>ID: </b>'.$someData['paymentId']."\n\r";
+                    $pay_text .= '<b>Сумма: </b>'.floatval($paymentInfo['amount']['value'])." ₽\n\r";
+                    $tgsender = new TelegramSendAction;
+                    $tgsender->handle($pay_text);
 
-
-                    Log::channel('pay')->info('Order status updated info: '. print_r($paymentInfo, true));
-                    Log::channel('pay')->info('Order status updated: '. $order->id);
+                    Log::channel('pay')->info('Order status updated info: '.print_r($paymentInfo, true));
+                    Log::channel('pay')->info('Order status updated: '.$order->id);
 
                 } else {
-                    Log::channel('pay')->info('Order not found: '. $someData['paymentId']);
+                    Log::channel('pay')->info('Order not found: '.$someData['paymentId']);
                 }
             } else {
                 header('HTTP/1.1 400 Something went wrong (info)');
                 exit();
             }
 
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             header('HTTP/1.1 400 Something went wrong (all)');
             exit();
         }
     }
 
-    protected function get_receipt($order, $tovars){
+    protected function get_receipt($order, $tovars)
+    {
         $receipt = [
             'tax_system_code' => 6,
             'items' => [],
             'customer' => [
                 'full_name' => $order->name,
                 'email' => $order->email,
-                'phone' => phone_format($order->phone),
+                'phone' => '+7'.phone_format($order->phone),
             ],
         ];
-
 
         foreach ($tovars as $product) {
             $receipt['items'][] = [
@@ -111,7 +107,7 @@ class YooKassaService {
                 'amount' => [
                     'value' => $product['price'].'.00',
                     'currency' => 'RUB',
-                ]
+                ],
             ];
         }
 
@@ -126,51 +122,54 @@ class YooKassaService {
                 'amount' => [
                     'value' => floatval($order->delivery_price).'.00',
                     'currency' => 'RUB',
-                ]
+                ],
             ];
         }
 
         return $receipt;
     }
 
-    public function getOrderStatus(string $pay_id) {
-        Log::channel('pay')->info('Get pay order staus: '. $pay_id);
+    public function getOrderStatus(string $pay_id)
+    {
+        Log::channel('pay')->info('Get pay order staus: '.$pay_id);
 
-        $client = new Client();
+        $client = new Client;
         $client->setAuth(config('yookassa.shop_id'), config('yookassa.secret_key'));
 
         try {
             return $client->getPaymentInfo($pay_id);
         } catch (\Exception $e) {
-            Log::channel('pay')->error('Get pay order staus FAILD: '. $e->getMessage());
+            Log::channel('pay')->error('Get pay order staus FAILD: '.$e->getMessage());
+
             return null;
         }
     }
 
-    public function registerOrder(Order $order, array $tovars) {
-        Log::channel('pay')->info('Pay create: '. $order->id);
+    public function registerOrder(Order $order, array $tovars)
+    {
+        Log::channel('pay')->info('Pay create: '.$order->id);
 
-        $client = new Client();
+        $client = new Client;
         $client->setAuth(config('yookassa.shop_id'), config('yookassa.secret_key'));
 
-        $pay_info = array(
-            'amount' => array(
+        $pay_info = [
+            'amount' => [
                 'value' => $order->amount.'.00',
                 'currency' => 'RUB',
-            ),
+            ],
             'receipt' => $this->get_receipt($order, $tovars),
-            'confirmation' => array(
+            'confirmation' => [
                 'type' => 'redirect',
-                'return_url' => route("bascet_thencs").'?order_id='.$order->id,
-            ),
+                'return_url' => route('bascet_thencs').'?order_id='.$order->id,
+            ],
             'capture' => true,
             'description' => 'Заказ №'.$order->id,
             'metadata' => [
-                'order_id' => $order->id
-            ]
-        );
+                'order_id' => $order->id,
+            ],
+        ];
 
-        Log::channel('pay')->info('Pay info: '. json_encode($pay_info));
+        Log::channel('pay')->info('Pay info: '.json_encode($pay_info));
 
         $payment = $client->createPayment(
             $pay_info,
@@ -179,5 +178,4 @@ class YooKassaService {
 
         return $payment;
     }
-
 }
