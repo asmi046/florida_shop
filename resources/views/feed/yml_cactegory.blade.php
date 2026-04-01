254002f7b8c09bdc79f038e2f5175f89f16953c2
asmi046 <asmi046@gmail.com>
2026-04-02 01:06:20 +0300

    Убрал разметку

--- resources/views/feed/yml_cactegory.blade.php
+++ resources/views/feed/yml_cactegory.blade.php
@@ -1,80 +1,85 @@
 @php
-    echo '<?xml version="1.0" encoding="UTF-8"?>';
-@endphp
+echo '<?xml version="1.0" encoding="UTF-8"@endphp';
+?>
 
-<yml_catalog date="{{ date("Y-m-d\TH:i:sP") }}">
-<shop>
-    <name>Florida</name>
-    <company>Доставка цветов "Флорида"</company>
-    <url>https://florida46.ru/</url>
-    <currencies>
-        <currency id="RUR" rate="1"/>
-    </currencies>
+<yml_catalog date="{{ date('Y-m-d\TH:i:sP') }}">
+    <shop>
+        <name>Florida</name>
+        <company>Доставка цветов "Флорида"</company>
+        <url>https://florida46.ru/</url>
+        <currencies>
+            <currency id="RUR" rate="1" />
+        </currencies>
 
-    <categories>
-        @foreach ($all_cat as $item)
-            <category id="{{$item->id}}">{{$item->title}}</category>
-        @endforeach
-    </categories>
+        <categories>
+            @foreach ($all_cat as $item)
+                <category id="{{ $item->id }}">{{ $item->title }}</category>
+            @endforeach
+        </categories>
 
-    <offers>
-        @foreach ($cat_product as $item)
-            @php
+        <offers>
+            @foreach ($cat_product as $item)
+                @php
 
-                if (empty($category)) {
-                    $cat = $item->tovar_categories()->first();
-                    if (!empty($cat))
-                        $cat = $cat["id"];
-                    else
+                    if (empty($category)) {
+                        $cat = $item->tovar_categories()->first();
+                        if (!empty($cat)) {
+                            $cat = $cat['id'];
+                        } else {
+                            continue;
+                        }
+                    } else {
+                        $cat = $category->id;
+                    }
+
+                    $consist = $item->consist;
+                    if (empty($consist)) {
                         continue;
-                } else {
-                    $cat = $category->id;
-                }
+                    }
+                @endphp
 
-                $consist = $item->consist;
-                if (empty($consist)) continue;
-            @endphp
+                <offer id="{{ $item->sku }}" available="{{ !$item->asc_nal ? 'true' : 'false' }}">
+                    <name>{{ $item->title }}</name>
+                    <url>{{ route('tovar', $item->slug) }}</url>
 
-            <offer id="{{$item->sku}}" available="{{(!$item->asc_nal)?"true":"false"}}">
-                <name>{{$item->title}}</name>
-                <url>{{route('tovar', $item->slug)}}</url>
+                    @if (Storage::disk('local')->exists('public/products_galery/' . $item->img))
+                        <picture>
+                            {{ (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] }}{{ Storage::url('public/products_galery/' . $item->img) }}
+                        </picture>
+                    @endif
 
-                @if(Storage::disk('local')->exists('public/products_galery/'.$item->img))
-                    <picture>{{((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') ."://". $_SERVER['HTTP_HOST']}}{{Storage::url('public/products_galery/'.$item->img)}}</picture>
-                @endif
+                    <picture>{{ route('home') . $item->img }}</picture>
 
-                <picture>{{route('home').$item->img}}</picture>
+                    <price>{{ $item->price }}</price>
 
-                <price>{{$item->price}}</price>
+                    @if (!empty($item->price_old))
+                        <oldprice>{{ $item->price_old }}</oldprice>
+                    @endif
 
-                @if (!empty($item->price_old))
-                    <oldprice>{{$item->price_old}}</oldprice>
-                @endif
+                    <description>{{ strip_tags($item->description) }}</description>
+                    <currencyId>RUR</currencyId>
 
-                <description>{{$item->description}}</description>
-                <currencyId>RUR</currencyId>
+                    <categoryId>{{ $cat }}</categoryId>
 
-                <categoryId>{{ $cat }}</categoryId>
-
-                @foreach ($consist as $elem)
-                    @continue(empty($elem['Имя']))
-                    <consist name="{{$elem['Имя']}}" unit="шт">{{$elem['Количество']}}</consist>
-                @endforeach
+                    @foreach ($consist as $elem)
+                        @continue(empty($elem['Имя']))
+                        <consist name="{{ $elem['Имя'] }}" unit="шт">{{ $elem['Количество'] }}</consist>
+                    @endforeach
 
 
 
-                <delivery>true</delivery>
-                <store>true</store>
-                <pickup>true</pickup>
-            </offer>
-        @endforeach
-    </offers>
-    <gifts>
-        <!-- подарки не из прайс‑листа -->
-    </gifts>
-    <promos>
-        <!-- промоакции -->
-    </promos>
-</shop>
+                    <delivery>true</delivery>
+                    <store>true</store>
+                    <pickup>true</pickup>
+                </offer>
+            @endforeach
+        </offers>
+        <gifts>
+            <!-- подарки не из прайс‑листа -->
+        </gifts>
+        <promos>
+            <!-- промоакции -->
+        </promos>
+    </shop>
 
 </yml_catalog>
