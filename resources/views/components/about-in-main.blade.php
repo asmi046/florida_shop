8ecee466059ceb609f20fe7a3e12980a5616e2df
asmi046 <asmi046@gmail.com>
2026-04-14 03:14:44 +0300

    Презентационное виде замена

--- resources/views/components/about-in-main.blade.php
+++ resources/views/components/about-in-main.blade.php
@@ -1,18 +1,18 @@
 <section id="about_main">
     <div class="_wrapper">
-            <div class="video_wraper">
-                <div class="mobile">
-                    <video autoplay muted loop width="250">
-                        <source src="{{ asset('video/present_video.mp4') }}" type="video/mp4" />
-                    </video>
-                    <div class="mb"></div>
-                    <div class="circl"></div>
-                </div>
+        <div class="video_wraper">
+            <div class="mobile">
+                <video autoplay muted loop width="250">
+                    <source src="{{ asset('video/florida46kursk.mp4') }}" type="video/mp4" />
+                </video>
+                <div class="mb"></div>
+                <div class="circl"></div>
             </div>
-            <div class="text_wrapper text_blk">
-                <h2>{{$options['main_h1']}}</h2>
+        </div>
+        <div class="text_wrapper text_blk">
+            <h2>{{ $options['main_h1'] }}</h2>
 
-                {!!$options['main_text']!!}
-            </div>
+            {!! $options['main_text'] !!}
+        </div>
     </div>
 </section>
