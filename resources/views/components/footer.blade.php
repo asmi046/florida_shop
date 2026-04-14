82317ee05c9d5eb2817a6ab2d133e719b54d9b71
asmi046 <asmi046@gmail.com>
2026-04-14 03:21:32 +0300

    Категория из подвала убрана

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,8 +2,8 @@
     <div class="_wrapper">
         <div class="cols">
             <div class="col">
-                <a class="logo" href="{{route('home')}}">
-                    <img src="{{asset('img/logo_new_white.svg')}}" alt="Florida - Курск">
+                <a class="logo" href="{{ route('home') }}">
+                    <img src="{{ asset('img/logo_new_white.svg') }}" alt="Florida - Курск">
                 </a>
             </div>
 
@@ -16,48 +16,52 @@
 
                 <ul>
                     @foreach ($all_cat as $cat)
-                        <li><a href="{{route('category', $cat->slug)}}">{{$cat->title}}</a></li>
+                        @continue(!$cat->in_main)
+                        <li><a href="{{ route('category', $cat->slug) }}">{{ $cat->title }}</a></li>
                     @endforeach
                 </ul>
             </div>
             <div class="col">
                 <h4>Особенности</h4>
                 <ul>
-                    <li><a href="{{route('hits')}}">Хит продаж</a></li>
-                    <li><a href="{{route('new_tovar')}}">Новинка</a></li>
-                    <li><a href="{{route('actions')}}">Скидки</a></li>
+                    <li><a href="{{ route('hits') }}">Хит продаж</a></li>
+                    <li><a href="{{ route('new_tovar') }}">Новинка</a></li>
+                    <li><a href="{{ route('actions') }}">Скидки</a></li>
                 </ul>
                 <h4>Аккаунт</h4>
 
                 <ul>
                     <li><a href="{{ route('bascet') }}">Корзина</a></li>
                     @auth('web')
-                        <li><a href="{{route('cabinet.home')}}">Кабинет</a></li>
+                        <li><a href="{{ route('cabinet.home') }}">Кабинет</a></li>
                     @endauth
 
                     @guest
-                        <li><a href="{{route('login')}}">Войти</a></li>
+                        <li><a href="{{ route('login') }}">Войти</a></li>
                     @endguest
                 </ul>
 
             </div>
             <div class="col">
                 <h4>Контакты</h4>
-                <a class="phone" href="tel:{{str_replace(array('-', ' ', '(' , ')'), '', $options['phone'])}}" class="phone">{{$options['phone']}}</a>
-                <p class="adress">{{$options['adress_fk']}}</p>
+                <a class="phone" href="tel:{{ str_replace(['-', ' ', '(', ')'], '', $options['phone']) }}"
+                    class="phone">{{ $options['phone'] }}</a>
+                <p class="adress">{{ $options['adress_fk'] }}</p>
 
                 <h4>Соцсети и мессенджеры</h4>
                 <x-messanger></x-messanger>
             </div>
         </div>
         <div class="policy_line">
-            <br/>
-            <a href="{{route("policy")}}">Политика в области обработки конфиденциальной информации и персональных данных</a> и <a href="{{route("policy_accept")}}">Согласие на обработку персональных данных</a>
-            <br/>
-            <br/>
-            <p>Индивидуальным предпринимателем Арепьев И.М. (Флорида)</p><br/>
-            <p>Курская область, Курский район, д. Зорино, ул. Пески д. 13</p><br/>
-            <a href="tel:{{str_replace(array('-', ' ', '(' , ')'), '', $options['phone'])}}" class="phone">{{$options['phone']}}</a><br/>
+            <br />
+            <a href="{{ route('policy') }}">Политика в области обработки конфиденциальной информации и персональных
+                данных</a> и <a href="{{ route('policy_accept') }}">Согласие на обработку персональных данных</a>
+            <br />
+            <br />
+            <p>Индивидуальным предпринимателем Арепьев И.М. (Флорида)</p><br />
+            <p>Курская область, Курский район, д. Зорино, ул. Пески д. 13</p><br />
+            <a href="tel:{{ str_replace(['-', ' ', '(', ')'], '', $options['phone']) }}"
+                class="phone">{{ $options['phone'] }}</a><br />
         </div>
     </div>
 
