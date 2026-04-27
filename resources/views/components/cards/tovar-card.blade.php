8ecbffe064669c0082ac5f25abf2bd7e34a2b167
asmi046 <asmi046@gmail.com>
2026-04-28 01:13:57 +0300

    loading="lazy"

--- resources/views/components/cards/tovar-card.blade.php
+++ resources/views/components/cards/tovar-card.blade.php
@@ -1,6 +1,8 @@
-
-<div data-prodid="{{$tovar['sku']}}" class="tovar_card_new">
-    <a href="{{ route('tovar', $tovar->slug) }}"  @class(['img_wrapper', 'has-second' => $tovar->product_images && count($tovar->product_images) > 0])>
+<div data-prodid="{{ $tovar['sku'] }}" class="tovar_card_new">
+    <a href="{{ route('tovar', $tovar->slug) }}" @class([
+        'img_wrapper',
+        'has-second' => $tovar->product_images && count($tovar->product_images) > 0,
+    ])>
         <div class="t_label_wrapper">
             @if ($tovar['old_price'])
                 <div class="t_label sale">sale</div>
@@ -15,9 +17,10 @@
             @endif
 
         </div>
-        <img class="tovar_img tovar_img_first" src="{{ $tovar->img }}" alt="{{ $tovar->title }}">
+        <img class="tovar_img tovar_img_first" loading="lazy" src="{{ $tovar->img }}" alt="{{ $tovar->title }}">
         @if ($tovar->product_images && count($tovar->product_images) > 0)
-            <img class="tovar_img tovar_img_second" loading="lazy" src="{{ $tovar->product_images[0]->link }}" alt="{{ $tovar->title }}">
+            <img class="tovar_img tovar_img_second" loading="lazy" src="{{ $tovar->product_images[0]->link }}"
+                alt="{{ $tovar->title }}">
         @endif
     </a>
     <p>{{ $tovar->title }}</p>
@@ -32,9 +35,9 @@
         </div>
 
         @if ($tovar->asc_nal)
-            <a href="#showModalNal_{{$tovar['sku']}}" class="button price_button">Уточнить наличие</a>
+            <a href="#showModalNal_{{ $tovar['sku'] }}" class="button price_button">Уточнить наличие</a>
         @else
-            <to-bascet-btn-page sku="{{$tovar['sku']}}"  :bascet="'/bascet'"></to-bascet-btn-page>
+            <to-bascet-btn-page sku="{{ $tovar['sku'] }}" :bascet="'/bascet'"></to-bascet-btn-page>
         @endif
     </div>
 </div>
@@ -83,4 +86,3 @@
     @endif
 
 </div> --}}
-
