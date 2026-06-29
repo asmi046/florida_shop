a581509e9d1b8175f74c646baa13973e854aaf6c
asmi046 <asmi046@gmail.com>
2026-06-29 15:41:51 +0300

    Теги с пагинацией и текстом

--- resources/views/tag.blade.php
+++ resources/views/tag.blade.php
@@ -5,27 +5,37 @@
 
 @section('content')
 
-<x-headers.header-inner :h1="$tag_info['alt_title']"></x-headers.header-inner>
+    <x-headers.header-inner :h1="$tag_info['alt_title']"></x-headers.header-inner>
 
-<section id="catalog_section" class="catalog_section">
-    <div class="_wrapper">
-         <div class="sitebar">
-            <x-tovar-filter></x-tovar-filter>
-        </div>
-        <div class="tovars_blk">
-            <div class="tovar_wrapper">
+    <section id="catalog_section" class="catalog_section">
+        <div class="_wrapper">
+            <div class="sitebar">
+                <x-tovar-filter></x-tovar-filter>
+            </div>
+            <div class="tovars_blk">
+                <div class="tovar_wrapper">
 
-                @foreach ($allproduct as $tovar)
-                    <x-cards.tovar-card :tovar="$tovar"></x-cards.tovar-card>
-                @endforeach
+                    @foreach ($allproduct as $tovar)
+                        <x-cards.tovar-card :tovar="$tovar"></x-cards.tovar-card>
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
-</section>
-<section class="category">
-    <div class="_wrapper">
-        <x-pagination :tovars="$allproduct"></x-pagination>
-    </div>
-</section>
+    </section>
+    <section class="category">
+        <div class="_wrapper">
+            <x-pagination :tovars="$allproduct"></x-pagination>
+        </div>
+    </section>
+
+    @if ($allproduct->onFirstPage())
+        <section class="category">
+            <div class="_wrapper">
+                <div class="text_styles category_text">
+                    {!! $tag_info->description !!}
+                </div>
+            </div>
+        </section>
+    @endif
 
 @endsection
