5e5c9e0d7a0adf8b21b7a0336a5b01541298a14c
asmi046 <asmi046@gmail.com>
2026-07-13 16:26:01 +0300

    Теги

--- app/Http/Controllers/ProductTagController.php
+++ app/Http/Controllers/ProductTagController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ProductTag;
-use Illuminate\Http\Request;
 use App\Filters\ProductFilter;
+use App\Models\ProductTag;
 
 class ProductTagController extends Controller
 {
-    public function show_tag (ProductFilter $request, $slug) {
+    public function show_tag(ProductFilter $request, $slug)
+    {
         $tagInfo = ProductTag::where('slug', $slug)->first();
 
-        if($tagInfo == null) abort('404');
+        if ($tagInfo == null) {
+            abort('404');
+        }
 
         $allproduct = $tagInfo->products()->filter($request)->paginate(9)->withQueryString();
+
+        if ($allproduct->currentPage() > $allproduct->lastPage() && $allproduct->total() > 0) {
+            abort(404);
+        }
 
         return view('tag', ['tag_info' => $tagInfo, 'allproduct' => $allproduct]);
     }
