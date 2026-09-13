6387ab121b1b5383c37f45a423e566ce1d1c5397
asmi046 <asmi046@gmail.com>
2026-09-13 23:54:18 +0300

    +1

--- resources/views/components/tovar-page-content/tovar-info.blade.php
+++ resources/views/components/tovar-page-content/tovar-info.blade.php
@@ -6,15 +6,17 @@
         </div>
 
         <div class="price_cerrecter">
-            <div class="price">{{$product['price']}} <span class="rub_symbol">₽</span></div>
+            <div class="price">{{ $product['price'] }} <span class="rub_symbol">₽</span></div>
             @if (!empty($product['old_price']))
-                <div class="old_price">{{$product['old_price']}} <span class="rub_symbol">₽</span></div>
+                <div class="old_price">{{ $product['old_price'] }} <span class="rub_symbol">₽</span></div>
             @endif
         </div>
     </div>
 
     <div class="like_blk">
-        <a href="#" onclick="navigator.share({'title': document.title, 'url':document.location.href}); return false;" class="all_control share pi florida_share"></a>
+        <a href="#"
+            onclick="navigator.share({'title': document.title, 'url':document.location.href}); return false;"
+            class="all_control share pi florida_share"></a>
         {{-- <a href="#" class="all_control favorites to_favorites pi florida_like" data-prodid="{{$product['sku']}}"></a> --}}
     </div>
 </div>
@@ -24,31 +26,32 @@
     @if ($product->asc_nal)
         <a href="#showModalNal" class="button card_to_bascet_btn">Уточнить наличие</a>
     @else
-        <to-bascet-btn-page sku="{{$product['sku']}}"  :bascet="'/bascet'"></to-bascet-btn-page>
+        <to-bascet-btn-page sku="{{ $product['sku'] }}" :bascet="'/bascet'"></to-bascet-btn-page>
     @endif
 
     <div class="quik_pricing_btn_wpap">
-        <a href="https://wa.me/{{str_replace(array('-', ' ', '(' , ')'), '', $options['phone'])}}" target="_blank" class="button">Написать в WhatsApp</a>
-        <a href="#ocbuy_{{$product->id}}" class="button">Быстрый заказ</a>
+        <a href="https://max.ru/u/f9LHodD0cOJWI1JquUJRpE7xfGtMgxS_TDlt1s0JUfLDqjj7fLt0ZYHiRgE" target="_blank"
+            class="button">Написать в Max</a>
+        <a href="#ocbuy_{{ $product->id }}" class="button">Быстрый заказ</a>
     </div>
 </div>
 
 <div class="pay_sens">
     <span>Принимаем к оплате:</span>
-    <img src="{{asset('img/icons/pay_sp.svg')}}" alt="">
+    <img src="{{ asset('img/icons/pay_sp.svg') }}" alt="">
 </div>
 
 <div class="tov_param_section">
     <h2>Параметры:</h2>
     <div class="param_blk">
         <div class="param param_height pi florida_param_height">
-            Высота:<br/>
-            <strong>{{$product['height']}}</strong>
+            Высота:<br />
+            <strong>{{ $product['height'] }}</strong>
         </div>
 
         <div class="param param_diam pi florida_param_diam">
-            Диаметр:<br/>
-            <strong>{{$product['radius']}}</strong>
+            Диаметр:<br />
+            <strong>{{ $product['radius'] }}</strong>
         </div>
     </div>
 </div>
